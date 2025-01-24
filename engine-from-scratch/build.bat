4490946a6247a4b8258446d692af0dcdc3de2fcc
TheSmith123 <ethan.m.smith0803@gmail.com>
2025-01-24 11:27:03 -0500

    implementing headers and functions

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -1,4 +1,4 @@
-set files="src\glad.c" "src\main.c"
+set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_init.c
 set libs="C:\Repos\my-game\lib\SDL2main.lib" "C:\Repos\my-game\lib\SDL2.lib" "C:\Repos\my-game\lib\freetype.lib"
 
 CL /Zi /I "C:\Repos\my-game\include" %files% /link %libs% /OUT:mygame.exe
