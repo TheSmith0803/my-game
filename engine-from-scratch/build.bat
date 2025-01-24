3575b8a735afcec3a7ceada177100f3b38f928e5
TheSmith123 <ethan.m.smith0803@gmail.com>
2025-01-24 10:23:47 -0500

    made paths in quotes

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -1,4 +1,4 @@
-set files=src\glad.c src\main.c
-set libs=C:\repos\my-game\lib\SDL2main.lib C:\repos\my-game\lib\SDL2.lib C:\repos\my-game\lib\freetype.lib
+set files="src\glad.c" "src\main.c"
+set libs="C:\Repos\my-game\lib\SDL2main.lib" "C:\Repos\my-game\lib\SDL2.lib" "C:\Repos\my-game\lib\freetype.lib"
 
-CL /Zi /I C:\repos\my-game\include %files% /link %libs% /OUT:mygame.exe
+CL /Zi /I "C:\Repos\my-game\include" %files% /link %libs% /OUT:mygame.exe
