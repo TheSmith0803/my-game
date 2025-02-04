9f2f66a73273cfdbc7b111ddbc388fb8ca9e9c62
TheSmith0803 <ethan.smith@ametek.com>
2025-02-04 07:52:11 -0500

    All the squares are flying of the screen...

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -3,7 +3,9 @@ set io=src\engine\io\io.c
 set config=src\engine\config.c 
 set input=src\engine\input.c 
 set time=src\engine\time.c 
-set files=src\glad.c src\main.c src\engine\global.c %render% %io% %config% %input% %time%
+set physics=src\engine\physics\physics.c 
+set array_list=src\engine\array_list.c 
+set files=src\glad.c src\main.c src\engine\global.c %render% %io% %config% %input% %time% %physics% %array_list%
 set libs=C:\Repos\my-game\lib\SDL2main.lib C:\Repos\my-game\lib\SDL2.lib 
 
 CL /Zi /I "C:\Repos\my-game\include" %files% /link %libs% /OUT:mygame.exe
