55c88dec2882a434181795bb42a38b5453a150eb
TheSmith123 <ethan.m.smith0803@gmail.com>
2025-02-14 22:46:54 -0500

    finished the minkowski difference

--- engine-from-scratch/src/engine/render/render_init.c
+++ engine-from-scratch/src/engine/render/render_init.c
@@ -21,8 +21,8 @@ SDL_Window *render_init_window(u32 width, u32 height) {
         "mygame",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        1920,
-        1080,
+        global.render.width,
+        global.render.height,
         SDL_WINDOW_OPENGL
     );
 
